f4687a122a72e611513aef6288391f725f67c174
Prosantodeb7 <prosantodeb7@gmail.com>
2021-01-30 15:56:09 +0600

    Home Page Added

--- src/project/Home.java
+++ src/project/Home.java
@@ -1,11 +1,72 @@
 
 package project;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import templates.Frame_Setup;
 
 
 public class Home extends Frame_Setup
 {
+    public JPanel button_panel;
+    public JButton normal_mode_button,cricket_mode_button;
+    
+    public Home()
+    {
+        setContainer();
+        setButtonPanel();
+        setButtons();
+    }
+    
+    public void setContainer()
+    {
+        //Method Overriding Contept Used
+        super.setContainer();  //Here we get setContainer() of parrent class
+        container.setLayout(new BorderLayout(100,100));
+        //container.setBackground(Color.white);
+    }
+    
+    public void setButtonPanel()
+    {
+        button_panel = new JPanel();
+        button_panel.setLayout(new GridLayout(2,1));
+        button_panel.setBackground(Color.WHITE);
+        container.add(button_panel,BorderLayout.CENTER);
+        setNullLabel();
+    }
+    
+    public void setButtons()
+    {
+        normal_mode_button = new JButton(normal_mode_icon);
+        normal_mode_button.setBackground(Color.WHITE);
+        normal_mode_button.setBorder(null);
+        normal_mode_button.setFocusPainted(false);
+        button_panel.add(normal_mode_button);
+        
+        cricket_mode_button = new JButton(cricket_mode_icon);
+        cricket_mode_button.setBackground(Color.WHITE);
+        cricket_mode_button.setBorder(null);
+        cricket_mode_button.setFocusPainted(false);
+        button_panel.add(cricket_mode_button);
+    }
+    
+    public void setNullLabel()
+    {
+        null_label = new JLabel();
+        container.add(null_label,BorderLayout.EAST);
+        null_label = new JLabel();
+        container.add(null_label,BorderLayout.WEST);
+        null_label = new JLabel();
+        container.add(null_label,BorderLayout.NORTH);
+        null_label = new JLabel();
+        container.add(null_label,BorderLayout.SOUTH);
+    }
+    
+    
     public static void main(String[] args)
     {
         Home frame = new Home();
--- src/templates/Frame_Setup.java
+++ src/templates/Frame_Setup.java
@@ -5,18 +5,46 @@ import java.awt.Color;
 import java.awt.Container;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Frame_Setup extends JFrame
 {
     public Container container;
+    public JLabel null_label;
     
+    public ImageIcon button_0_icon = new ImageIcon(getClass().getResource("/icons/button_0.png"));
+    public ImageIcon button_1_icon = new ImageIcon(getClass().getResource("/icons/button_1.png"));
+    public ImageIcon button_2_icon = new ImageIcon(getClass().getResource("/icons/button_2.png"));
+    public ImageIcon button_3_icon = new ImageIcon(getClass().getResource("/icons/button_3.png"));
+    public ImageIcon button_4_icon = new ImageIcon(getClass().getResource("/icons/button_4.png"));
+    public ImageIcon button_5_icon = new ImageIcon(getClass().getResource("/icons/button_5.png"));
+    public ImageIcon button_6_icon = new ImageIcon(getClass().getResource("/icons/button_6.png"));
+    public ImageIcon button_7_icon = new ImageIcon(getClass().getResource("/icons/button_7.png"));
+    public ImageIcon button_8_icon = new ImageIcon(getClass().getResource("/icons/button_8.png"));
+    public ImageIcon button_9_icon = new ImageIcon(getClass().getResource("/icons/button_9.png"));
+    public ImageIcon button_point_icon = new ImageIcon(getClass().getResource("/icons/button_point.png"));
+    public ImageIcon button_equal_icon = new ImageIcon(getClass().getResource("/icons/button_equal.png"));
+    public ImageIcon button_plus_icon = new ImageIcon(getClass().getResource("/icons/button_plus.png"));
+    public ImageIcon button_minus_icon = new ImageIcon(getClass().getResource("/icons/button_minus.png"));
+    public ImageIcon button_minus_sign_icon = new ImageIcon(getClass().getResource("/icons/button_minus_sign.png"));
+    public ImageIcon button_mul_icon = new ImageIcon(getClass().getResource("/icons/button_mul.png"));
+    public ImageIcon button_div_icon = new ImageIcon(getClass().getResource("/icons/button_div.png"));
+    public ImageIcon button_clr_icon = new ImageIcon(getClass().getResource("/icons/button_clr.png"));
+    public ImageIcon button_back_icon = new ImageIcon(getClass().getResource("/icons/button_back.png"));
+    public ImageIcon button_home_icon = new ImageIcon(getClass().getResource("/icons/button_home.png"));
+    public ImageIcon button_wide_ball_icon = new ImageIcon(getClass().getResource("/icons/button_wide_ball.png"));
+    public ImageIcon button_no_ball_icon = new ImageIcon(getClass().getResource("/icons/button_no_ball.png"));
+    public ImageIcon button_wicket_icon = new ImageIcon(getClass().getResource("/icons/button_wicket.png"));
+    public ImageIcon button_refresh_icon = new ImageIcon(getClass().getResource("/icons/button_refresh.png"));
+    public ImageIcon normal_mode_icon = new ImageIcon(getClass().getResource("/icons/button_normal_mode.png"));
+    public ImageIcon cricket_mode_icon = new ImageIcon(getClass().getResource("/icons/button_cricket_mode.png"));
+
     
     public Frame_Setup()
     {
         setFrame();
         setContainer();
-        setAllIconsDeclared();
         setAppIcon();
     }
     
@@ -31,36 +59,6 @@ public class Frame_Setup extends JFrame
     {
         container = this.getContentPane();
         container.setBackground(Color.white);
-        container.setLayout(null);
-    }
-    
-    public void setAllIconsDeclared()
-    {
-        ImageIcon button_0_icon = new ImageIcon(getClass().getResource("/icons/button_0.png"));
-        ImageIcon button_1_icon = new ImageIcon(getClass().getResource("/icons/button_1.png"));
-        ImageIcon button_2_icon = new ImageIcon(getClass().getResource("/icons/button_2.png"));
-        ImageIcon button_3_icon = new ImageIcon(getClass().getResource("/icons/button_3.png"));
-        ImageIcon button_4_icon = new ImageIcon(getClass().getResource("/icons/button_4.png"));
-        ImageIcon button_5_icon = new ImageIcon(getClass().getResource("/icons/button_5.png"));
-        ImageIcon button_6_icon = new ImageIcon(getClass().getResource("/icons/button_6.png"));
-        ImageIcon button_7_icon = new ImageIcon(getClass().getResource("/icons/button_7.png"));
-        ImageIcon button_8_icon = new ImageIcon(getClass().getResource("/icons/button_8.png"));
-        ImageIcon button_9_icon = new ImageIcon(getClass().getResource("/icons/button_9.png"));
-        ImageIcon button_point_icon = new ImageIcon(getClass().getResource("/icons/button_point.png"));
-        ImageIcon button_equal_icon = new ImageIcon(getClass().getResource("/icons/button_equal.png"));
-        ImageIcon button_plus_icon = new ImageIcon(getClass().getResource("/icons/button_plus.png"));
-        ImageIcon button_minus_icon = new ImageIcon(getClass().getResource("/icons/button_minus.png"));
-        ImageIcon button_minus_sign_icon = new ImageIcon(getClass().getResource("/icons/button_minus_sign.png"));
-        ImageIcon button_mul_icon = new ImageIcon(getClass().getResource("/icons/button_mul.png"));
-        ImageIcon button_div_icon = new ImageIcon(getClass().getResource("/icons/button_div.png"));
-        ImageIcon button_clr_icon = new ImageIcon(getClass().getResource("/icons/button_clr.png"));
-        ImageIcon button_back_icon = new ImageIcon(getClass().getResource("/icons/button_back.png"));
-        ImageIcon button_home_icon = new ImageIcon(getClass().getResource("/icons/button_home.png"));
-        ImageIcon button_wide_ball_icon = new ImageIcon(getClass().getResource("/icons/button_wide_ball.png"));
-        ImageIcon button_no_ball_icon = new ImageIcon(getClass().getResource("/icons/button_no_ball.png"));
-        ImageIcon button_wicket_icon = new ImageIcon(getClass().getResource("/icons/button_wicket.png"));
-        ImageIcon button_refresh_icon = new ImageIcon(getClass().getResource("/icons/button_refresh.png"));
-           
     }
     
     public void setAppIcon()
